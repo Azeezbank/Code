941c86cf0b187c9e204ec269e99eff0525fdde57
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2024-02-21 22:18:54 +0100

    user_input.py

---
+++ user_input.py
@@ -0,0 +1,11 @@
+name = input("What is your name?: ")
+print(name)
+age = input("How old are you?: ")
+print(age)
+location = input("Where are you from?: ")
+print(location)
+
+Name = "Bankole"
+Age = 30
+Location = "Nigeria"
+print('Hello {}, you are {} years old, and you are living in {}'.format(Name,Age,Location))
